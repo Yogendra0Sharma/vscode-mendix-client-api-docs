a13648be2dd34f318f2f0628681163fdfc98b126
Yogendra Sharma <eh.yogendra@gmail.com>
2020-06-30 16:08:20 +0530

    Add Images

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,12 @@ export function activate(context: vscode.ExtensionContext) {
 	let url8 = 'https://apidocs.mendix.com/8/client';
 	let url7 = 'https://apidocs.mendix.com/7/client';
 
-	let mendix8ClientAPI = vscode.commands.registerCommand('mendixwidgetDoc.MendixClient8API', () => {
+	let mendix8ClientAPI = vscode.commands.registerCommand('extension.mendixClient8API', () => {
 		vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(url8));
 	});
 	context.subscriptions.push(mendix8ClientAPI);
 
-	let mendix7ClientAPI = vscode.commands.registerCommand('mendixwidgetDoc.MendixClient7API', () => {
+	let mendix7ClientAPI = vscode.commands.registerCommand('extension.mendixClient7API', () => {
 		vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(url7));
 	});
 	context.subscriptions.push(mendix7ClientAPI);
